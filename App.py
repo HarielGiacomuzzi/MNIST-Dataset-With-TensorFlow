18709b45a4c9ed9b2185ab53ab5c74208308d7d0
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2016-07-01 10:35:15 -0300

    training done

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import math
 import numpy
+import time
 from tensorflow.python.framework import dtypes
 import collections
 
@@ -12,6 +13,8 @@ NUM_CLASSES = 10
 IMAGE_SIZE = 28
 IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 BATCH_SIZE = 100
+MAX_STEPS = 3000
+TRAIN_DIR = 'data'
 
 Datasets = collections.namedtuple('Datasets', ['train', 'validation', 'test'])
 
@@ -115,11 +118,90 @@ def run_training():
 		sess = tf.Session()
 
 		# Instantiate a SummaryWriter to output summaries and the Graph.
-		summary_writer = tf.train.SummaryWriter('data', sess.graph)
+		summary_writer = tf.train.SummaryWriter(TRAIN_DIR, sess.graph)
 		
 		# And then after everything is built:
 		# Run the Op to initialize the variables.
 		sess.run(init)
+
+		# Start the training loop.
+		for step in xrange(MAX_STEPS):
+			start_time = time.time()
+
+			# Fill a feed dictionary with the actual set of images and labels
+			# for this particular training step.
+			feed_dict = fill_feed_dict( data_sets.train, images_placeholder, labels_placeholder )
+
+			# Run one step of the model.  The return values are the activations
+			# from the `train_op` (which is discarded) and the `loss` Op.  To
+			# inspect the values of your Ops or variables, you may include them
+			# in the list passed to sess.run() and the value tensors will be
+			# returned in the tuple from the call.
+			_, loss_value = sess.run([train_op, loss],feed_dict=feed_dict)
+			duration = time.time() - start_time
+
+			# Write the summaries and print an overview fairly often.
+			if step % 100 == 0:
+				# Print status to stdout.
+				print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+				# Update the events file.
+				summary_str = sess.run(summary_op, feed_dict=feed_dict)
+				summary_writer.add_summary(summary_str, step)
+				summary_writer.flush()
+
+
+			# Save a checkpoint and evaluate the model periodically.
+			if (step + 1) % 1000 == 0 or (step + 1) == MAX_STEPS:
+				saver.save(sess, TRAIN_DIR, global_step=step)
+				# Evaluate against the training set.
+				print('Training Data Eval:')
+				do_eval(sess,eval_correct,images_placeholder,labels_placeholder,data_sets.train)
+				# Evaluate against the validation set.
+				print('Validation Data Eval:')
+				do_eval(sess,eval_correct,images_placeholder,labels_placeholder,data_sets.validation)
+				# Evaluate against the test set.
+				print('Test Data Eval:')
+				do_eval(sess,eval_correct,images_placeholder,labels_placeholder,data_sets.test)
+
+def do_eval(sess,eval_correct,images_placeholder,labels_placeholder,data_set):
+	"""Runs one evaluation against the full epoch of data.
+	Args:
+	sess: The session in which the model has been trained.
+	eval_correct: The Tensor that returns the number of correct predictions.
+	images_placeholder: The images placeholder.
+	labels_placeholder: The labels placeholder.
+	data_set: The set of images and labels to evaluate, from
+	input_data.read_data_sets().
+	"""
+	# And run one epoch of eval.
+	true_count = 0  # Counts the number of correct predictions.
+	steps_per_epoch = data_set.num_examples // BATCH_SIZE
+	num_examples = steps_per_epoch * BATCH_SIZE
+	for step in xrange(steps_per_epoch):
+		feed_dict = fill_feed_dict(data_set,images_placeholder,labels_placeholder)
+		true_count += sess.run(eval_correct, feed_dict=feed_dict)
+	precision = true_count / num_examples
+	print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.08f' %(num_examples, true_count, precision))
+
+def fill_feed_dict(data_set, images_pl, labels_pl):
+	"""Fills the feed_dict for training the given step.
+	A feed_dict takes the form of:
+	feed_dict = {
+	<placeholder>: <tensor of values to be passed for placeholder>,
+	....
+	}
+	Args:
+	data_set: The set of images and labels, from input_data.read_data_sets()
+	images_pl: The images placeholder, from placeholder_inputs().
+	labels_pl: The labels placeholder, from placeholder_inputs().
+	Returns:
+	feed_dict: The feed dictionary mapping from placeholders to values.
+	"""
+	# Create the feed_dict for the placeholders filled with the next
+	# `batch size ` examples.
+	images_feed, labels_feed = data_set.next_batch( BATCH_SIZE, False )
+	feed_dict = { images_pl: images_feed, labels_pl: labels_feed, }
+	return feed_dict
 
 # reads 32 bits from a binary file
 def _read32(bytestream):
